98fab849199746dd1fdfc615c5d1946c07d0e229
Moisés Lima <moises.silva3000@outlook.com>
2023-01-30 23:15:51 -0300

    Ajuste layout tela cadastro

--- src/modules/cadastro-pessoa/views/CadastroPessoa.vue
+++ src/modules/cadastro-pessoa/views/CadastroPessoa.vue
@@ -1,8 +1,13 @@
 <template>
   <div>
-    <card>
-      <h2>Teste</h2>
-    </card>
+    <base-header class="pb-6 pb-8 pt-5 pt-md-8 bg-gradient-success">
+      <card>
+        <h2>Teste</h2>
+      </card>
+    </base-header>
+
+    <!--Main-->
+    <b-container fluid class="mt--7" style="min-height: 700px"> </b-container>
   </div>
 </template>
 <script>
