7796730b565b30b05cede4679793d6042a2ae35a
Moisés Lima <moises.silva3000@outlook.com>
2023-01-30 23:02:35 -0300

    finalizada configuração para o projeto trabalhar em modules

--- src/main.js
+++ src/main.js
@@ -1,22 +1,37 @@
-// import Vue from "vue";
 import Vue from "vue/dist/vue.js";
 import VueRouter from "vue-router";
-import router from "./modules/core/routes/router";
 
 import DashboardPlugin from "@/modules/core/plugins/dashboard-plugin";
 
 import CoreModule from "./modules/core/core-modules.js";
 import CadastroPessoa from "./modules/cadastro-pessoa/cadastro-pessoa-module";
+import routes from "./modules/core/routes/routes";
 
 Vue.use(VueRouter);
 Vue.use(DashboardPlugin);
 
-function bootstrap() {
-  const cadastroPessoa = new CadastroPessoa(router);
-  cadastroPessoa.install();
+(function modules() {
+  const cadastroPessoa = new CadastroPessoa();
+  cadastroPessoa.install(routes);
+})();
 
+// configure router
+const router = new VueRouter({
+  mode: "history",
+  routes, // short for routes: routes
+  linkActiveClass: "active",
+  scrollBehavior: (to, from, savedPosition) => {
+    if (savedPosition) {
+      return savedPosition;
+    }
+    if (to.hash) {
+      return { selector: to.hash };
+    }
+    return { x: 0, y: 0 };
+  },
+});
+
+(function coreModule() {
   const coreModule = new CoreModule(router);
   coreModule.install(Vue);
-}
-
-bootstrap();
+})();
