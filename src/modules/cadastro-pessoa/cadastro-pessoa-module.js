b273ee99dd3e240656bca6be1ed618572b8ab5f5
Moisés Lima <moises.silva3000@outlook.com>
2023-02-04 17:27:50 -0300

    Retirado import nao utilizado

--- src/modules/cadastro-pessoa/cadastro-pessoa-module.js
+++ src/modules/cadastro-pessoa/cadastro-pessoa-module.js
@@ -1,6 +1,4 @@
 import cadastroPessoaRoute from "@/modules/cadastro-pessoa/router";
-import routes from "../core/routes/routes";
-
 export default class CadastroPessoa {
   install(routes) {
     for (const route of cadastroPessoaRoute) {
