1609d03b17737d340779f143e62f5ab7ae873c39
Moisés Lima <moises.silva3000@outlook.com>
2023-04-09 19:53:48 -0300

    Iniciados testes do cadastro de pessoa

--- src/modules/core/store/index.js
+++ src/modules/core/store/index.js
@@ -1,12 +1,6 @@
 import state from "./state";
-import mutation from "./mutations";
-import mutationTypes from "./mutations-types";
-import * as actions from "./actions";
 
 export default {
   state,
-  mutation,
-  mutationTypes,
-  actions,
   namespaced: true,
 };
