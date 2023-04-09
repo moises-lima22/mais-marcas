81a7fecbe53126bda4683a45fad2a59cfef9135b
Moisés Lima <moises.silva3000@outlook.com>
2023-04-09 18:34:21 -0300

    Configurado Vue Test Utils

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  preset: "@vue/cli-plugin-unit-jest",
+  testMatch: [
+    "**/__tests__/**/*.spec.[jt]s?(x)",
+    "**/tests/unit/**/*.spec.[jt]s?(x)",
+  ],
+  transformIgnorePatterns: ["/node_modules/"], // Pastas que devem ser ignoradas na transformação
+  collectCoverage: true, // Habilita a coleta de cobertura de código
+  coverageReporters: ["html", "text-summary"], // Formatos de relatórios de cobertura
+  coverageDirectory: "<rootDir>/coverage", // Pasta onde os relatórios de cobertura serão gerados
+  collectCoverageFrom: [
+    "src/**/*.{js,jsx,ts,tsx,vue}",
+    "!src/modules/core/**",
+    "!src/main.js", // Arquivo de entrada principal, normalmente não precisa ser coberto
+    "!src/router/**", // Arquivos de configuração do router, normalmente não precisam ser cobertos
+    "!**/node_modules/**", // Pastas de node_modules, normalmente não precisam ser cobertas
+  ],
+};
---
+++ tests/unit/modules/cadastro-pessoa/example.spec.js
@@ -0,0 +1,12 @@
+import { shallowMount } from "@vue/test-utils";
+import HelloWorld from "@/modules/cadastro-pessoa/views/Teste.vue";
+
+describe("HelloWorld.vue", () => {
+  it("renders props.msg when passed", () => {
+    const msg = "new message";
+    const wrapper = shallowMount(HelloWorld, {
+      propsData: { msg },
+    });
+    expect(wrapper.text()).toMatch(msg);
+  });
+});
