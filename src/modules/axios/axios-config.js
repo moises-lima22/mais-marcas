0bcfb2ac4a022661f2bb00bffe4b02cb2f7d42e9
Moisés Lima <moises.silva3000@outlook.com>
2023-05-10 18:03:20 -0300

    Ajuste no axios

--- src/modules/axios/axios-config.js
+++ src/modules/axios/axios-config.js
@@ -3,4 +3,10 @@ import axios from "axios";
 // Configura a URL base da API
 axios.defaults.baseURL = process.env.VUE_APP_BASE_URL;
 
+const _accessToken = localStorage.getItem("accessToken");
+
+if (_accessToken) {
+  axios.defaults.headers.common["Authorization"] = `Bearer ${_accessToken}`;
+}
+
 export default axios;
