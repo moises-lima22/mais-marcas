9370fb5ae6ca0a51559c6b65ee8832308eed7bfd
Moisés Lima <moises.silva3000@outlook.com>
2023-07-05 13:55:02 -0300

    troca de pc

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import App from "@/modules/core/App.vue";
+import { createPinia, PiniaVuePlugin } from "pinia";
 
 import DashboardPlugin from "@/modules/core/plugins/dashboard-plugin";
 
@@ -8,8 +9,12 @@ import store from "./modules/store";
 
 Vue.use(DashboardPlugin);
 
+Vue.use(PiniaVuePlugin);
+const pinia = createPinia();
+
 new Vue({
   router: router,
   store: store,
+  pinia,
   render: (h) => h(App),
 }).$mount("#app");
